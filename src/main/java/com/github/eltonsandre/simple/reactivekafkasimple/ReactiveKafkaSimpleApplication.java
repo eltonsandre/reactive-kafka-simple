c07cd3d6f757e2a90fbc116f7831bc5e4810b802
eltonsandre <eltonsandre@gmail.com>
2021-11-02 13:51:41 -0300

    scaffold

---
+++ src/main/java/com/github/eltonsandre/simple/reactivekafkasimple/ReactiveKafkaSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.github.eltonsandre.simple.reactivekafkasimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveKafkaSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveKafkaSimpleApplication.class, args);
+	}
+
+}
